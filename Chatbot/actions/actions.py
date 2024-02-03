9fa7894c288ba90bbb03be09a6c76cb3642f39ab
M. Farhaan baig <116679001+fourhaan@users.noreply.github.com>
2024-02-03 21:53:36 +0530

    Update actions.py

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -29,10 +29,17 @@
 from typing import Dict, Text, Any, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import re
+import requests
 
 class ActionSendData(Action):
     def name(self) -> Text:
         return "action_send_data"
+
+    def is_valid_email(self, email: Text) -> bool:
+        # Regular expression for a basic email validation
+        email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        return bool(re.match(email_regex, email))
 
     def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
@@ -40,7 +47,37 @@ class ActionSendData(Action):
         name = tracker.get_slot("name")
         email = tracker.get_slot("email")
 
-        dispatcher.utter_message(f"Thank you, {name}! Your email address is {email}.")
+        if email and self.is_valid_email(email):
+            dispatcher.utter_message("An OTP has been sent to your email address, kindly enter the 6 digit OTP")
+        else:
+            dispatcher.utter_message("I'm sorry, the provided email address is not valid. Please provide a valid email.")
 
         return []
+
+class ActionValidOtp(Action):
+    def name(self) -> Text:
+        return "action_valid_otp"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        otp = tracker.get_slot("otp")
+
+        if otp and len(otp) == 6:
+            # Send OTP to Flask API
+            # flask_api_url = "http://your-flask-api-url"
+            # data = {"otp": otp}
+            # response = requests.post(f"{flask_api_url}/receive_otp", json=data)
+
+            # if response.ok:
+            #     dispatcher.utter_message("OTP sent to Flask API successfully.")
+            # else:
+            #     dispatcher.utter_message("Error sending OTP to Flask API.")
+            dispatcher.utter_message(f"OTP = {otp} successfully verified")
+
+        else:
+            dispatcher.utter_message("Invalid OTP format. Please provide a 6-digit OTP.")
+
+        return []
+
 
